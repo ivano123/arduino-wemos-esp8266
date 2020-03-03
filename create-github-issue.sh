f994372e99d36ff2ca286b3542e0c0c85f398965
mshaposhnikov <max@shaposhnikoff.info>
2020-03-03 11:13:56 -0800

    2020-03-03-111356 I __ a word

--- create-github-issue.sh
+++ create-github-issue.sh
@@ -15,5 +15,5 @@ curl -X "POST" "https://api.github.com/repos/shaposhnikoff/arduino-wemos-esp8266
      -H "Content-Type: text/plain; charset=utf-8" \
      -d $'{
   "title": "Alert '${PLATFORMIO_CI_SRC}'",
-  "body": "'${TRAVIS_JOB_ID}' \r\n The command %s exited with ${result}. \r\n travis job id - '${TRAVIS_JOB_ID}' \r\n travis build web url  '${TRAVIS_BUILD_WEB_URL}' \r\n  travis job web url  '${TRAVIS_JOB_WEB_URL}'"
+  "body": "Job id is '${TRAVIS_JOB_ID}' \r\n The command %s exited with ${result}. \r\n travis job id - '${TRAVIS_JOB_ID}' \r\n travis build web url  '${TRAVIS_BUILD_WEB_URL}' \r\n  travis job web url  '${TRAVIS_JOB_WEB_URL}'"
 }'
