68bac76cdb7e414f30dad827508ff2dfa0f7f170
mshaposhnikov <max@shaposhnikoff.info>
2020-03-02 22:11:06 -0800

    2020-03-02-221105 starting the service is always better

--- create-github-issue.sh
+++ create-github-issue.sh
@@ -2,6 +2,7 @@
 
 
 #export TRAVIS_JOB_ID=10101010101
+#export TRAVIS_BUILD_WEB_URL="https://kjsdhfkjshdkfjshdfkjhs.com"
 echo $TRAVIS_JOB_ID
 
 curl -X "POST" "https://api.github.com/repos/shaposhnikoff/arduino-wemos-esp8266/issues?state=all" \
@@ -9,6 +10,6 @@ curl -X "POST" "https://api.github.com/repos/shaposhnikoff/arduino-wemos-esp8266
      -H "Authorization: token $gh_token" \
      -H "Content-Type: text/plain; charset=utf-8" \
      -d $'{
-  "title": "A workflow alerts Operator who performs a process",
-  "body": "\\n'${TRAVIS_JOB_ID}' \\n The\\n command %s exited with ${result}.'${TRAVIS_JOB_ID}' \\n"
+  "title": "A workflow alerts Operator who performs a process '${TRAVIS_JOB_ID}'",
+  "body": "'${TRAVIS_JOB_ID}' The command %s exited with ${result}.'${TRAVIS_JOB_ID}' travis build web url  '${TRAVIS_BUILD_WEB_URL}'  travis job web url  '${TRAVIS_JOB_WEB_URL}'"
 }'
