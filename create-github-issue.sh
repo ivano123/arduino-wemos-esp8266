31d7b3dfe7d730b0e24ee6ccf054a6479438a6fa
mshaposhnikov <max@shaposhnikoff.info>
2020-03-02 21:50:44 -0800

    2020-03-02-215044 better code

--- create-github-issue.sh
+++ create-github-issue.sh
@@ -7,5 +7,5 @@ curl -X "POST" "https://api.github.com/repos/shaposhnikoff/arduino-wemos-esp8266
      -H "Content-Type: text/plain; charset=utf-8" \
      -d $'{
   "title": "A workflow alerts Operator who performs a process",
-  "body": "\\n${TRAVIS_JOB_ID}The \n\n\n\n\n command %s exited with ${result}.${TRAVIS_JOB_ID}\\n"
+  "body": "\\n${TRAVIS_JOB_ID}The \\n command %s exited with ${result}.${TRAVIS_JOB_ID}\\n"
 }'
