3b21403f37529bc18e5fce881110ddceaeb27713
mshaposhnikov <max@shaposhnikoff.info>
2020-03-03 12:15:39 -0800

    2020-03-03-121539 Refactor factories, revisit visitors

--- create-github-issue.sh
+++ create-github-issue.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 
 #export TRAVIS_JOB_ID=10101010101
@@ -6,15 +6,34 @@
 #export TRAVIS_BUILD_WEB_URL="https://kjsdhfjkshdfkjshf.com"
 #export TRAVIS_JOB_WEB_URL="https://jksdhfksdhfjksdhfk.sfsfsdf.sdfsfsdfs.ru"
 
-
 echo $TRAVIS_JOB_ID
+
+
+
+
+BUCKET_NAME=testbucket
+OBJECT_NAME=testworkflow-2.0.1.jar
+TARGET_LOCATION=/opt/test/testworkflow-2.0.1.jar
+
+JSON_STRING=$( jq -n \
+                  --arg bn "$BUCKET_NAME" \
+                  --arg on "$OBJECT_NAME" \
+                  --arg tl "$TARGET_LOCATION" \
+                  '{bucketname: $bn, objectname: $on, targetlocation: $tl}' )
+
+echo $JSON_STRING
+
+exit 0
+
 
 curl -X "POST" "https://api.github.com/repos/shaposhnikoff/arduino-wemos-esp8266/issues?state=all" \
      -H "Cookie: logged_in=no" \
      -H "Authorization: token $gh_token" \
      -H "Content-Type: text/plain; charset=utf-8" \
+     -H "Accept: application/vnd.github.v3.text+json" \
      -d $'{
   "title": "Alert '${PLATFORMIO_CI_SRC}'",
+  "labels": ["bug"],
   "body": "Job id is '${TRAVIS_JOB_ID}' \r\n Build was failed with ${result}. \r\n travis job id - '${TRAVIS_JOB_ID}' \r\n\n travis build web url \r\n '${TRAVIS_BUILD_WEB_URL}' \r\n\n  travis job web url \r\n '${TRAVIS_JOB_WEB_URL}'"
 }'
 
--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash 
 
 
+
 export PLATFORMIO_CI_SRC=esp8266/GSM_auto/GSM_auto.ino 
 export PLATFORMIO_CI_BOARDS_ARGS=--board=d1
 
 
-bash -c 'platformio ci --lib="." $PLATFORMIO_CI_BOARDS_ARGS' > file.log 2>&1
+#bash -c 'platformio ci --lib="." $PLATFORMIO_CI_BOARDS_ARGS' > file.log 2>&1
+
+input="file.log"
+
+while IFS= read -r line
+do
+  echo "{" $line "}"
+done < "$input"
 
