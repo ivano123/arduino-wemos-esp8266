bec5337a87dd96e27a20fd7ec1d06b84b137fe3c
mshaposhnikov <max@shaposhnikoff.info>
2020-03-03 11:34:30 -0800

    2020-03-03-113413 You should have trusted me.

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+
+export PLATFORMIO_CI_SRC=esp8266/GSM_auto/GSM_auto.ino 
+export PLATFORMIO_CI_BOARDS_ARGS=--board=d1
+
+
+bash -c 'platformio ci --lib="." $PLATFORMIO_CI_BOARDS_ARGS' > file.log 2>&1
+
