d740a4a9e7bfd146496f41f2e8fd321c3aa8973e
mshaposhnikov <max@shaposhnikoff.info>
2020-03-02 21:36:41 -0800

    2020-03-02-213641 Ok, 5am, it works.  For real.

--- create-github-issue.sh
+++ create-github-issue.sh
@@ -6,6 +6,6 @@ curl -X "POST" "https://api.github.com/repos/shaposhnikoff/arduino-wemos-esp8266
      -H "Authorization: token $gh_token" \
      -H "Content-Type: text/plain; charset=utf-8" \
      -d $'{
-  "title": "A workflow alerts Operator who performs a process $TRAVIS_BUILD_WEB_URL",
-  "body": $TRAVIS_BUILD_WEB_URL
+  "title": "A workflow alerts Operator who performs a process",
+  "body": "${TRAVIS_WHEREAMI_URL:-https://whereami.travis-ci.com}"
 }'
