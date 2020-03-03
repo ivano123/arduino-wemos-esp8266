77d2d6dac092ca583309b1edd7fea6b850eca848
mshaposhnikov <max@shaposhnikoff.info>
2020-03-03 11:16:47 -0800

    2020-03-03-111646 ffs

--- create-github-issue.sh
+++ create-github-issue.sh
@@ -15,5 +15,6 @@ curl -X "POST" "https://api.github.com/repos/shaposhnikoff/arduino-wemos-esp8266
      -H "Content-Type: text/plain; charset=utf-8" \
      -d $'{
   "title": "Alert '${PLATFORMIO_CI_SRC}'",
-  "body": "Job id is '${TRAVIS_JOB_ID}' \r\n The command %s exited with ${result}. \r\n travis job id - '${TRAVIS_JOB_ID}' \r\n travis build web url  '${TRAVIS_BUILD_WEB_URL}' \r\n  travis job web url  '${TRAVIS_JOB_WEB_URL}'"
+  "body": "Job id is '${TRAVIS_JOB_ID}' \r\n Build was failed with ${result}. \r\n travis job id - '${TRAVIS_JOB_ID}' \r\n\n travis build web url \r\n '${TRAVIS_BUILD_WEB_URL}' \r\n\n  travis job web url \r\n '${TRAVIS_JOB_WEB_URL}'"
 }'
+
